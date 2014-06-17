3fc2f8e54542e6f85f8e21815ad044a0b17428bd
Matt Dunbar <mdunbar@demacmedia.com>
2014-06-17 15:49:43 -0400

    Converted 404 test to expect EE base template instead of CE.

--- tests/frontend/404.js
+++ tests/frontend/404.js
@@ -7,7 +7,7 @@ casper.test.begin('404 Not Found', function suite(test) {
 
         test.assertTitle('404 Not Found 1');
         test.assertExists('body.cms-index-noroute');
-        test.assertSelectorHasText('.page-head-alt', 'Whoops, our bad...');
+        test.assertSelectorHasText('.page-head-alt', 'We are sorry, but the page you are looking for cannot be found.');
     })
 
     .run(function () {
